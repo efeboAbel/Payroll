b4d2af871ac232ee29dd43035a4808876765fa78
Omar Francisco Rodriguez Morales <omar_rodriguez@siman.com>
2026-04-15 14:47:16 -0600

    Adicion de EmployeeNotFoundAdvice.java

---
+++ src/main/java/com/example/Payroll/controller/EmployeeNotFoundAdvice.java
@@ -0,0 +1,17 @@
+package com.example.Payroll.controller;
+
+import com.example.Payroll.exceptions.EmployeeNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+class EmployeeNotFoundAdvice {
+
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String employeeNotFoundHandler(EmployeeNotFoundException ex) {
+        return ex.getMessage();
+    }
+}
