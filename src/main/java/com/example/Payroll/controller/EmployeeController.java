9f143d8b9126fcb68d1c30220b43094c663b3664
Omar Francisco Rodriguez Morales <omar_rodriguez@siman.com>
2026-04-15 16:34:33 -0600

    Cambios en el controller, y una excepcion

--- src/main/java/com/example/Payroll/controller/EmployeeController.java
+++ src/main/java/com/example/Payroll/controller/EmployeeController.java
@@ -1,10 +1,12 @@
 package com.example.Payroll.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.example.Payroll.entities.Employee;
 import com.example.Payroll.exceptions.EmployeeNotFoundException;
 import com.example.Payroll.repositories.EmployeeRepository;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.hateoas.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 class EmployeeController {
@@ -24,10 +29,10 @@ class EmployeeController {
 
     // Aggregate root
     // tag::get-aggregate-root[]
-    @GetMapping("/employees")
+    /*@GetMapping("/employees")
     List<Employee> all() {
         return repository.findAll();
-    }
+    }*/
     // end::get-aggregate-root[]
 
     @PostMapping("/employees")
@@ -36,10 +41,10 @@ class EmployeeController {
     }
 
     // Single item
-    @GetMapping("/employees/{id}")
+   /* @GetMapping("/employees/{id}")
     Employee one(@PathVariable Long id) {
         return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
-    }
+    }*/
 
     @PutMapping("/employees/{id}")
     Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
@@ -58,5 +63,27 @@ class EmployeeController {
     @DeleteMapping("/employees/{id}")
     void deleteEmployee(@PathVariable Long id) {
         repository.deleteById(id);
+    }
+
+    @GetMapping("/employees/{id}")
+    EntityModel<Employee> one(@PathVariable Long id) {
+
+        Employee employee = repository.findById(id) //
+                .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+        return EntityModel.of(employee, //
+                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+    }
+
+    @GetMapping("/employees")
+    CollectionModel<EntityModel<Employee>> all() {
+        List<EntityModel<Employee>> employees = repository.findAll().stream()
+                .map(employee -> EntityModel.of(employee,
+                        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
 }
--- src/main/java/com/example/Payroll/controller/EmployeeNotFoundAdvice.java
+++ src/main/java/com/example/Payroll/controller/EmployeeNotFoundAdvice.java
@@ -1,6 +1,7 @@
 package com.example.Payroll.controller;
 
 import com.example.Payroll.exceptions.EmployeeNotFoundException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
