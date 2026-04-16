9f44421fbb55212cf12363b50734e732e4968d8c
Omar Francisco Rodriguez Morales <omar_rodriguez@siman.com>
2026-04-15 19:20:14 -0600

    Cambios en Employee.java, EmployeeController.java y se creó EmployeeModelAssembler.java

---
+++ src/main/java/com/example/Payroll/component/EmployeeModelAssembler.java
@@ -0,0 +1,22 @@
+package com.example.Payroll.component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import com.example.Payroll.controller.EmployeeController;
+import com.example.Payroll.entities.Employee;
+
+@Component
+public class EmployeeModelAssembler implements RepresentationModelAssembler<Employee, EntityModel<Employee>> {
+
+    @Override
+    public EntityModel<Employee> toModel(Employee employee) {
+
+        return EntityModel.of(employee, //
+                linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+    }
+}
--- src/main/java/com/example/Payroll/controller/EmployeeController.java
+++ src/main/java/com/example/Payroll/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import com.example.Payroll.entities.Employee;
 import com.example.Payroll.exceptions.EmployeeNotFoundException;
 import com.example.Payroll.repositories.EmployeeRepository;
+import com.example.Payroll.component.EmployeeModelAssembler;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,13 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.hateoas.*;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
+
 @RestController
-class EmployeeController {
+public class EmployeeController {
 
     private final EmployeeRepository repository;
+    private final EmployeeModelAssembler assembler;
 
-    EmployeeController(EmployeeRepository repository) {
+    EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler) {
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     // Aggregate root
@@ -35,10 +41,10 @@ class EmployeeController {
     }*/
     // end::get-aggregate-root[]
 
-    @PostMapping("/employees")
+    /*@PostMapping("/employees")
     Employee newEmployee(@RequestBody Employee newEmployee) {
         return repository.save(newEmployee);
-    }
+    }*/
 
     // Single item
    /* @GetMapping("/employees/{id}")
@@ -66,18 +72,16 @@ class EmployeeController {
     }
 
     @GetMapping("/employees/{id}")
-    EntityModel<Employee> one(@PathVariable Long id) {
+    public EntityModel<Employee> one(@PathVariable Long id) {
+        Employee employee = repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
 
-        Employee employee = repository.findById(id) //
-                .orElseThrow(() -> new EmployeeNotFoundException(id));
-
-        return EntityModel.of(employee, //
+        return EntityModel.of(employee,
                 linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
                 linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
     }
 
     @GetMapping("/employees")
-    CollectionModel<EntityModel<Employee>> all() {
+    public CollectionModel<EntityModel<Employee>> all() {
         List<EntityModel<Employee>> employees = repository.findAll().stream()
                 .map(employee -> EntityModel.of(employee,
                         linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
@@ -85,5 +89,15 @@ class EmployeeController {
                 .collect(Collectors.toList());
 
         return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
+    }
+
+    @PostMapping("/employees")
+    ResponseEntity<?> newEmployee(@RequestBody Employee newEmployee) {
+
+        EntityModel<Employee> entityModel = assembler.toModel(repository.save(newEmployee));
+
+        return ResponseEntity //
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()) //
+                .body(entityModel);
     }
 }
